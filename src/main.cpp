dd3d3622cc83da857f8b297d516aab74b85c8126
yhur <yoonseok@gmail.com>
2021-09-13 09:20:58 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,15 @@
 const int   LED = 2;
 
 void setup() {
-  // put your setup code here, to run once:
+  
   pinMode(LED, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED, !digitalRead(LED));
+  
+  digitalWrite(LED, HIGH);
+  delay(1000);
+  
+  digitalWrite(LED, LOW);
   delay(1000);
 }
